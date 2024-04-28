72497748a1c381e1a500e589719ac489da197248
MykolaServetnyk <mikokobass@gmail.com>
2024-04-28 10:59:56 +0300

    setting default img

--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -26,20 +26,22 @@ export default function MovieCast() {
         fetchMovieCast();
     }, [movieId]);
 
+    const defaultImg = 'https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg'
     return (
         <div>
             {isLoading && <p>Loading...</p>}
             {error && <p>Please try again later...</p>}
         <ul className={css.castList}>
-            {moviesCast.length > 0 && moviesCast.map(({ credit_id, profile_path, original_name, character }) => {
+                {moviesCast.length > 0 ?
+                    moviesCast.map(({ credit_id, profile_path, original_name, character }) => {
                 return (
                     <li key={credit_id}>
-                        <img className={css.img} src={`https://image.tmdb.org/t/p/w200${profile_path}`} alt={original_name} />
+                        <img className={css.img} src={profile_path ? `https://image.tmdb.org/t/p/w200${profile_path}` : defaultImg} alt={original_name} width={200} />
                         <h2 className={css.castTitle}><RiUserStarFill /> {original_name}</h2>
                         <p className={css.castChar}>"{character}"</p>
                     </li>
                 );
-            })}
+            }): <p>No info provided</p>}
             </ul>
            
         </div>
--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -33,7 +33,7 @@ export default function MovieDetailsPage() {
     fetchMovie();
   }, [movieId]);
 
-  //const defaultImg = '<https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg>'
+  const defaultImg = 'https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg'
     
     return (
       <div>
@@ -46,7 +46,7 @@ export default function MovieDetailsPage() {
             {error && <p>Something went wrong. Please try again later...</p>}
             {!isLoading && !error && (
               <div className={css.movieDet}>
-                <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+                <img src={movie.poster_path?`https://image.tmdb.org/t/p/w500${movie.poster_path}`:defaultImg} alt={movie.title} />
                 <div className={css.movieDesc}>
                   <h1>{movie.title}</h1>
                 <p><span className={css.subtitle}>Overview:</span><br></br>{movie.overview}</p>
